051a912cd1983ef352cc59c42952f653bd6518a5
dlpduy <dinhlephucduy@gmail.com>
2025-01-03 22:26:12 +0700

    update

--- src/main/java/com/project/bee_rushtech/configs/CORSConfiguration.java
+++ src/main/java/com/project/bee_rushtech/configs/CORSConfiguration.java
@@ -29,18 +29,15 @@ public class CORSConfiguration {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("https://bee-rush-tech-mflhp0o2e-dlpduys-projects.vercel.app")); // Allow
-                                                                                                                       // all
-                                                                                                                       // origins
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE",
-                "OPTIONS")); // Allowed methods
-        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type",
-                "Accept"));
-        configuration.setAllowCredentials(true); // Allow credentials
+        configuration.setAllowedOrigins(Arrays.asList(
+                "https://bee-rush-tech-fe-git-main-dlpduys-projects.vercel.app",
+                "http://localhost:3000"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Accept"));
+        configuration.setAllowCredentials(true);
         configuration.setMaxAge(3600L);
-        // How long the response from a pre-flight request can be cached by clients
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration); // Apply this configuration to all paths
+        source.registerCorsConfiguration("/**", configuration);
         return source;
     }
 }
